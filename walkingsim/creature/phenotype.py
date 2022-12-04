93391d9c2ef5e68519893822a26d5d11baeb6620
Antoine de Frahan <antoine.defrahan@protonmail.com>
2022-12-04 22:14:55 +0100

    Revert "fix(phenotype): disable collision for bodies in same phenotype"
    This reverts commit 8879ba1fac86a3fae7139d98bf45754ec8e6ff5a.

--- walkingsim/creature/phenotype.py
+++ walkingsim/creature/phenotype.py
@@ -34,8 +34,8 @@ class Phenotype:
                 parent_part = bone.Bone(
                     self.genotype.nodes[node]["dimensions"], chrono.ChVectorD(0, 1.9, 0)
                 )
-                #  parent_part.SetBodyFixed(True)
-                parent_part.GetCollisionModel().SetFamily(1)
+                parent_part.SetBodyFixed(True)
+                parent_part.GetCollisionModel().SetFamilyGroup(0)
                 self.bones.append(parent_part)
                 self.env.Add(parent_part)
             for edge in self.genotype.edges(nbunch=node, data=True):
@@ -49,8 +49,7 @@ class Phenotype:
                 child_part_pos = new_joint_pos
                 child_part_pos[1] -= dimensions[1] / 2
                 child_part = bone.Bone(dimensions, chrono.ChVectorD(*child_part_pos))
-                child_part.GetCollisionModel().SetFamily(1)
-                child_part.GetCollisionModel().SetFamilyMaskNoCollisionWithFamily(1)
+                child_part.GetCollisionModel().SetFamilyGroup(0)
                 self.bones.append(child_part)
                 self.env.Add(child_part)
                 new_joint.Initialize(parent_part, child_part, joint_frame)
