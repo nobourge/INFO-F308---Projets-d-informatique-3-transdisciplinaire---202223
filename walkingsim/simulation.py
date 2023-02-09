7be7e291fa85d5852bed681baaf6270751d507f0
Antoine de Frahan <antoine.defrahan@protonmail.com>
2023-02-09 17:45:49 +0100

    fix(simulation): Simulation ABC on standby

--- walkingsim/simulation.py
+++ walkingsim/simulation.py
@@ -20,68 +20,68 @@ from walkingsim.creature.quadrupede import Quadrupede
 from walkingsim.environment import EnvironmentLoader
 
 
-class Simulation(abc.ABC):
-    """Abstract class used to create simulations. This class is used by
-    `ChronoSimulation`.
-
-    This class initiates the `EnvironmentLoader` and the `CreatureGenerator`
-    for the given engine. It also loads the given environment.
-
-    :var environment: The environment system specific to the engine
-    :var generator: The creature generator specific to the engine
-    :var creature: The creature in the simulation
-    :var genome: The genome of the creature
-    """
-
-    def __init__(
-        self,
-        __engine: str,
-        __env_datapath: str,
-        __env: str,
-        __creatures_datapath: str,
-        __visualize: bool = False,
-    ) -> None:
-        self.__engine = __engine
-        self.__loader = EnvironmentLoader(__env_datapath, self.__engine)
-        self._visualize = __visualize
-        self.__environment = self.__loader.load_environment(__env)
-
-        self.__creature = None
-        self.__genome = None
-
-    def add_creature(self, creature_name: str, genome: dict = None):
-        # FIXME: This function can be removed and done in the __init__ method
-        if self.__creature is not None:
-            logger.error(
-                "Cannot add a new creature to the simulation, one already exists !"
-            )
-            raise RuntimeError("Creature already exists in simulation")
-
-        # FIXME: Pass the genome when creating the creature
-        creature = Quadrupede((0, 1.9, 0))
-        creature.add_to_env(self.environment)
-        self.__creature = creature
-        self.__genome = genome
-        logger.debug(f"Creature '{creature}' added to the simulation")
-
-    @property
-    def environment(self):
-        return self.__environment
-
-    @property
-    def generator(self):
-        return self.__generator
-
-    @property
-    def creature(self):
-        return self.__creature
-
-    @property
-    def genome(self):
-        return self.__genome
-
-    def run(self):
-        raise NotImplementedError
+#  class Simulation(abc.ABC):
+#      """Abstract class used to create simulations. This class is used by
+#      `ChronoSimulation`.
+#
+#      This class initiates the `EnvironmentLoader` and the `CreatureGenerator`
+#      for the given engine. It also loads the given environment.
+#
+#      :var environment: The environment system specific to the engine
+#      :var generator: The creature generator specific to the engine
+#      :var creature: The creature in the simulation
+#      :var genome: The genome of the creature
+#      """
+#
+#      def __init__(
+#          self,
+#          __engine: str,
+#          __env_datapath: str,
+#          __env: str,
+#          __creatures_datapath: str,
+#          __visualize: bool = False,
+#      ) -> None:
+#          self.__engine = __engine
+#          self.__loader = EnvironmentLoader(__env_datapath, self.__engine)
+#          self._visualize = __visualize
+#          self.__environment = self.__loader.load_environment(__env)
+#
+#          self.__creature = None
+#          self.__genome = None
+#
+#      def add_creature(self, creature_name: str, genome: dict = None):
+#          # FIXME: This function can be removed and done in the __init__ method
+#          if self.__creature is not None:
+#              logger.error(
+#                  "Cannot add a new creature to the simulation, one already exists !"
+#              )
+#              raise RuntimeError("Creature already exists in simulation")
+#
+#          # FIXME: Pass the genome when creating the creature
+#          creature = Quadrupede((0, 1.9, 0))
+#          creature.add_to_env(self.environment)
+#          self.__creature = creature
+#          self.__genome = genome
+#          logger.debug(f"Creature '{creature}' added to the simulation")
+#
+#      @property
+#      def environment(self):
+#          return self.__environment
+#
+#      @property
+#      def generator(self):
+#          return self.__generator
+#
+#      @property
+#      def creature(self):
+#          return self.__creature
+#
+#      @property
+#      def genome(self):
+#          return self.__genome
+#
+#      def run(self):
+#          raise NotImplementedError
 
 
 class ChronoSimulation(Simulation):
@@ -121,6 +121,9 @@ class ChronoSimulation(Simulation):
         self.__renderer.EndScene()
 
     # Run Simulation
+    def _compute_step_reward(self):
+        pass
+
     def _simulation_step(self):
         """This function returns wether or not the simulation is done, and the
         result (fitness) of this simulation.
