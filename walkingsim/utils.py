0eb46d19d05384dcb95b6ce9f07118dda494435d
Antoine de Frahan <antoine.defrahan@protonmail.com>
2023-02-09 11:39:17 +0100

    feat(utils): utility functions file

---
+++ walkingsim/utils.py
@@ -0,0 +1,19 @@
+"""
+3D PyChrono muscle-based walking simulator
+File: utils.py
+Authors:
+    BECKER Robin-Gilles
+    BOURGEOIS Noé
+    HENRY DE FRAHAN Antoine
+    PALMISANO Luca
+Description:
+    Utility functions.
+"""
+
+import math
+
+
+def _distance(a, b):
+    return math.sqrt(
+        (a[0] + b[0]) ** 2 + (a[1] + b[1]) ** 2 + (a[2] + b[2]) ** 2
+    )
