4dc36f7ef604753da35ce2603773e5d350b1e0fa
Lucino772 <lucapalmi772@gmail.com>
2023-02-13 21:20:59 +0100

    chore(logging): do not show DEBUG logs by default

--- utils/_logging.py
+++ utils/_logging.py
@@ -2,4 +2,9 @@
 # For more info checkout the documetation at:
 # https://loguru.readthedocs.io/en/stable/index.html
 
+import os
+
+# Do not show DEBUG messages
+os.environ["LOGURU_LEVEL"] = "INFO"
+
 from loguru import logger
