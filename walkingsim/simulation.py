5b02dd40b3e34dc941dfc7ffdb70897e5300d520
Dark Peace <74102789+Dark-Peace@users.noreply.github.com>
2023-02-15 20:45:44 +0100

    Fixed bugs in simul & env

--- walkingsim/simulation.py
+++ walkingsim/simulation.py
@@ -21,71 +21,70 @@ from walkingsim.creature.bipede import Bipede
 from walkingsim.creature.quadrupede import Quadrupede
 from walkingsim.environment import EnvironmentLoader
 
-#  class Simulation(abc.ABC):
-#      """Abstract class used to create simulations. This class is used by
-#      `ChronoSimulation`.
-#
-#      This class initiates the `EnvironmentLoader` and the `CreatureGenerator`
-#      for the given engine. It also loads the given environment.
-#
-#      :var environment: The environment system specific to the engine
-#      :var generator: The creature generator specific to the engine
-#      :var creature: The creature in the simulation
-#      :var genome: The genome of the creature
-#      """
-#
-#      def __init__(
-#          self,
-#          __engine: str,
-#          __env_datapath: str,
-#          __env: str,
-#          __creatures_datapath: str,
-#          __visualize: bool = False,
-#      ) -> None:
-#          self.__engine = __engine
-#          self.__loader = EnvironmentLoader(__env_datapath, self.__engine)
-#          self._visualize = __visualize
-#          self.__environment = self.__loader.load_environment(__env)
-#
-#          self.__creature = None
-#          self.__genome = None
-#
-#      def add_creature(self, creature_name: str, genome: dict = None):
-#          # FIXME: This function can be removed and done in the __init__ method
-#          if self.__creature is not None:
-#              logger.error(
-#                  "Cannot add a new creature to the simulation, one already exists !"
-#              )
-#              raise RuntimeError("Creature already exists in simulation")
-#
-#          # FIXME: Pass the genome when creating the creature
-#          creature = Quadrupede((0, 1.9, 0))
-#          creature.add_to_env(self.environment)
-#          self.__creature = creature
-#          self.__genome = genome
-#          logger.debug(f"Creature '{creature}' added to the simulation")
-#
-#      @property
-#      def environment(self):
-#          return self.__environment
-#
-#      @property
-#      def generator(self):
-#          return self.__generator
-#
-#      @property
-#      def creature(self):
-#          return self.__creature
-#
-#      @property
-#      def genome(self):
-#          return self.__genome
-#
-#      def run(self):
-#          raise NotImplementedError
+class Simulation(abc.ABC):
+    """Abstract class used to create simulations. This class is used by
+    `ChronoSimulation`.
 
+    This class initiates the `EnvironmentLoader` and the `CreatureGenerator`
+    for the given engine. It also loads the given environment.
 
-class ChronoSimulation:
+    :var environment: The environment system specific to the engine
+    :var generator: The creature generator specific to the engine
+    :var creature: The creature in the simulation
+    :var genome: The genome of the creature
+    """
+
+    def __init__(
+        self,
+        __engine: str,
+        __env_datapath: str,
+        __env: str,
+        __creatures_datapath: str,
+        __visualize: bool = False,
+    ) -> None:
+        self.__engine = __engine
+        self.__loader = EnvironmentLoader(__env_datapath, self.__engine)
+        self._visualize = __visualize
+        #self.__environment = self.__loader.load_environment(__env)
+        self.__creature = None
+        self.__genome = None
+        self.add_creature("")
+
+    def add_creature(self, creature_name: str, genome: dict = None):
+        #FIXME: This function can be removed and done in the __init__ method
+        if self.__creature is not None:
+            logger.error(
+                "Cannot add a new creature to the simulation, one already exists !"
+            )
+            raise RuntimeError("Creature already exists in simulation")
+
+        # FIXME: Pass the genome when creating the creature
+        new_creature = Quadrupede((0, 1.9, 0))
+        new_creature.add_to_env(self.environment)
+        self.__creature = new_creature
+        self.__genome = genome
+        logger.debug(f"Creature '{new_creature}' added to the simulation")
+
+    @property
+    def environment(self):
+        return self.__environment
+
+    @property
+    def generator(self):
+        return self.__generator
+
+    @property
+    def creature(self):
+        return self.__creature
+
+    @property
+    def genome(self):
+        return self.__genome
+
+    def run(self):
+        raise NotImplementedError
+
+class ChronoSimulation(Simulation):
     """
     Simulation class for `chrono`.
     The genome used for the simulation is an m*n matrix, where
@@ -132,15 +131,7 @@ class ChronoSimulation:
         if self._visualize is True:
             # FIXME use ChIrrApp to have a GUI and tweak parameters within rendering
             self.__renderer = chronoirr.ChVisualSystemIrrlicht()
-
-        # Creature attributes
-        movement_matrix = np.array(__movement_gene).reshape(
-            Quadrupede._nb_joints, self._GENOME_DISCRETE_INTERVALS
-        )
-        self.__creature = Quadrupede((0, 2.4, 0), movement_matrix)
-        self.__creature.add_to_env(self.environment)
-        self.__creature.set_forces(movement_matrix)
-        self.__total_reward = 0
+        super().__init__(__env_datapath, __env, __creatures_datapath,"", __visualize)
 
     # Visualize
     def _render_setup(self):
@@ -221,12 +212,12 @@ class ChronoSimulation:
     def is_creature_fallen(self):
         # FIXME hacky. Is there a way to detect collision between shapes?
         try:
-            trunk_y = self.__creature.sensor_data[-1]["position"][1]
+            print(self.creature)
+            trunk_y = self.creature.sensor_data[-1]["position"][1]
         except IndexError:
             trunk_y = 1
 
-        height_limit = self.__creature._trunk_dimensions[2] / 2
-        # print(f"trunk y: {trunk_y}, trunk height: {height_limit}")
+        height_limit = self.creature._trunk_dimensions[2] / 2
         return trunk_y < 1.2 * height_limit
 
     def run(self):
@@ -258,4 +249,4 @@ class ChronoSimulation:
 
     @property
     def creature(self):
-        return self.__creature
+        return super().creature
