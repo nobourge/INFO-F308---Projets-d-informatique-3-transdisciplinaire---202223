4f805ce4531202944fa9ff676a13970c05fb2d66
Lucino772 <lucapalmi772@gmail.com>
2023-02-20 16:42:47 +0100

    refactor: removed old progress bar utils

--- walkingsim/utils/progress.py
+++ walkingsim/utils/progress.py
@@ -1,35 +1,8 @@
 import typing as t
-from contextlib import contextmanager
-from contextvars import ContextVar
 
 import tqdm
-from tqdm.contrib.logging import logging_redirect_tqdm
 
 _PROGRESSBARS: t.MutableMapping[str, tqdm.tqdm] = {}
-
-_PROGRESSBAR_VAR = ContextVar[tqdm.tqdm]("_PROGRESSBAR_VAR")
-
-
-@contextmanager
-def progress_bar(*args, **kwargs):
-    with logging_redirect_tqdm():
-        ctx_token = None
-        try:
-            _bar = tqdm.tqdm(*args, **kwargs)
-            ctx_token = _PROGRESSBAR_VAR.set(_bar)
-            yield _bar
-        finally:
-            _PROGRESSBAR_VAR.reset(ctx_token)
-            _bar.close()
-
-
-from loguru import logger
-
-
-def update_bar(i):
-    _bar = _PROGRESSBAR_VAR.get(None)
-    if _bar is not None:
-        _bar.update(i)
 
 
 def create_pb(__name: str, *args, **kwargs):
