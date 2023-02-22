2dd297c10ec5dc2fbe0eef5882022378fe4866f0
Dark Peace <74102789+Dark-Peace@users.noreply.github.com>
2023-02-22 10:38:34 +0100

    parent_selection_type = "tournament"
    slight improve in results

--- walkingsim/algorithms/ga.py
+++ walkingsim/algorithms/ga.py
@@ -38,6 +38,8 @@ class GeneticAlgorithm:
             on_mutation=self._on_mutation,
             on_stop=self._on_stop,
             parallel_processing=10,  # quantity of cores to use
+            init_range_low=0,
+            parent_selection_type = "tournament",
         )
 
     @staticmethod
