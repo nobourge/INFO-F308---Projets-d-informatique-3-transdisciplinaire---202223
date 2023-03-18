e2c2e9221ef4a979d988a4efe6370c37fc983cc2
Antoine de Frahan <antoine.defrahan@outlook.com>
2023-03-18 12:18:04 +0100

    feat(textures): new textures

--- walkingsim/envs/chrono/visualizer.py
+++ walkingsim/envs/chrono/visualizer.py
@@ -12,7 +12,7 @@ class ChronoVisualizer:
         self.__visualizer.SetWindowSize(1024, 768)
         self.__visualizer.SetWindowTitle("3D muscle-based walking sim")
         self.__visualizer.Initialize()
-        self.__visualizer.AddSkyBox("resources/materials/skybox/pink_sky/")
+        self.__visualizer.AddSkyBox("resources/materials/skybox/space/")
         self.__visualizer.AddCamera(chrono.ChVectorD(11, 3, 12))
         self.__visualizer.AddTypicalLights()
         #  self.__visualizer.SetShadows(True)
