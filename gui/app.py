77a41d9d28043f6cfde0778a522b0bb76bf24f75
Antoine de Frahan <antoine.defrahan@outlook.com>
2023-03-19 11:00:34 +0100

    feat(gui): main tab nav

--- gui/app.py
+++ gui/app.py
@@ -5,7 +5,19 @@ from tkinter import ttk
 class App:
     def __init__(self):
         self.__root = Tk()        
-        self.__mainframe = ttk.Frame(self.__root, padding="3 3 12 12")
+        # Widget creation
+        self.__content = ttk.Frame(self.__root, padding="3 3 12 12")
+        self.__win_title = ttk.Label(self.__content, text="Walking simulator")
+        self.__main_tabs = ttk.Notebook(self.__root)
+        self.__sim_tab = ttk.Frame(self.__main_tabs)
+        self.__vis_tab = ttk.Frame(self.__main_tabs)
+        self.__main_tabs.add(self.__sim_tab, text='Simulation')
+        self.__main_tabs.add(self.__vis_tab, text='Visualisation')
+        #  self.__mainframe = ttk.Frame(self.__content,)
+        # Widget grid placement
+        self.__content.grid(column=0, row=0)
+        self.__win_title.grid(column=0, row=0)
+        self.__main_tabs.grid(column=0, row=1, columnspan=2, rowspan=1)
 
     def run(self):
         self.__root.mainloop()
