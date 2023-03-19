6cb80d4101f69e3ff711f962b7e54ab28231e0d7
Antoine de Frahan <antoine.defrahan@outlook.com>
2023-03-19 10:41:29 +0100

    feat(gui): tkinter window

--- gui/app.py
+++ gui/app.py
@@ -4,5 +4,12 @@ from tkinter import ttk
 
 class App:
     def __init__(self):
-        pass
-        
+        self.__root = Tk()        
+        self.__mainframe = ttk.Frame(self.__root, padding="3 3 12 12")
+
+    def run(self):
+        self.__root.mainloop()
+
+
+if __name__ == "__main__":
+    app = App().run()
