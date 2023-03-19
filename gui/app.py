ebcb1e5305b50d33aaf0f43c56a5d51b780f54b3
Antoine de Frahan <antoine.defrahan@outlook.com>
2023-03-19 11:33:05 +0100

    fix(gui): stretching grid

--- gui/app.py
+++ gui/app.py
@@ -6,10 +6,12 @@ class App:
     def __init__(self):
         self.__root = Tk()
         self.__root.title("3D walking simulator")
+        self.__root.columnconfigure(0, weight=1)
+        #  self.__root.rowconfigure(0, weight=1)
         # Widget creation
         self.__content = ttk.Frame(self.__root, padding="3 3 12 12")
         self.__win_title = ttk.Label(self.__content, text="3D Walking simulator")
-        self.__main_tabs = ttk.Notebook(self.__root)
+        self.__main_tabs = ttk.Notebook(self.__content)
         self.__sim_tab = ttk.Frame(self.__main_tabs)
         self.__vis_tab = ttk.Frame(self.__main_tabs)
         self.__main_tabs.add(self.__sim_tab, text='Simulation')
@@ -19,6 +21,11 @@ class App:
         self.__content.grid(column=0, row=0)
         self.__win_title.grid(column=0, row=0)
         self.__main_tabs.grid(column=0, row=1, columnspan=2, rowspan=1)
+
+        # Debug borders
+        for e in self.__content, self.__win_title:
+            e["borderwidth"] = 3
+            e["relief"] = "solid" 
 
     def run(self):
         self.__root.mainloop()
