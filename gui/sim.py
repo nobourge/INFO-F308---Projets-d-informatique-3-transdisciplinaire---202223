7994101e2cc964b3b0888683f5ad4eddf45a3378
Antoine de Frahan <antoine.defrahan@outlook.com>
2023-03-19 18:59:54 +0100

    refactor(gui): sim widgets as attributes

--- gui/sim.py
+++ gui/sim.py
@@ -10,38 +10,42 @@ class SimView(ttk.Frame):
 
     def _setup(self):
         # Environment selection
-        env_select_label = ttk.Label(self, text="Environnement")
-        #  self._add_debug_borders(env_select_label)
-        env_select_label.grid(row=0, column=0, padx="0 10", sticky=W)
+        self._env_select_label = ttk.Label(self, text="Environnement")
+        #  self._add_debug_borders(self._env_select_label)
+        self._env_select_label.grid(row=0, column=0, padx="0 10", sticky=W)
         self.__env_var = StringVar()
-        env_select = ttk.Combobox(
-            self, textvariable=self.__env_var
-        )
-        env_select["values"] = ["default", "lune", "mars"]
-        env_select.grid(row=0, column=1, sticky=(W, E))
+        self._env_select_field = ttk.Combobox(
+                self, textvariable=self.__env_var
+                )
+        self._env_select_field["values"] = ["default", "lune", "mars"]
+        self._env_select_field.grid(row=0, column=1, sticky=(W, E))
         
         # Creature config
-        creature_select_lbl = ttk.Label(self, text="Choix de la créature")
-        creature_select_lbl.grid(row=1, column=0, padx="0 10", sticky=W)
+        self._creature_select_lbl = ttk.Label(self, text="Choix de la créature")
+        self._creature_select_lbl.grid(row=1, column=0, padx="0 10", sticky=W)
         self.__creature_var = StringVar()
-        creature_select = ttk.Combobox(
+        self._creature_select_field = ttk.Combobox(
             self, textvariable=self.__creature_var
         )
-        creature_select["values"] = ["quadrupède", "bipède"]
-        creature_select.grid(row=1, column=1, sticky=(W, E))
+        self._creature_select_field["values"] = ["quadrupède", "bipède"]
+        self._creature_select_field.grid(row=1, column=1, sticky=(W, E))
 
         # Algorithm config
         algo_config_lbl = ttk.Label(self, text="Configuration algorithme d'apprentissage")
         #  self._add_debug_borders(algo_config_lbl)
-        algo_config_lbl.grid(row=2, column=0, padx="0 10", columnspan=2, sticky=W)
-        algo_select_lbl = ttk.Label(self, text="Choix de l'algorithme")
-        algo_select_lbl.grid(row=3, column=0, padx="0 10", sticky=W)
+        self._algo_config_lbl.grid(row=2, column=0, padx="0 10", columnspan=2, sticky=W)
+        self._algo_select_lbl = ttk.Label(self, text="Choix de l'algorithme")
+        self._algo_select_lbl.grid(row=3, column=0, padx="0 10", sticky=W)
         self.__algo_var = StringVar()
-        algo_select = ttk.Combobox(
+        self._algo_select_field = ttk.Combobox(
             self, textvariable=self.__algo_var
         )
-        algo_select["values"] = ["Algorithme génétique", "PPO"]
-        algo_select.grid(row=3, column=1, sticky=(W, E))
+        self._algo_select_field["values"] = ["Algorithme génétique", "PPO"]
+        self._algo_select_field.grid(row=3, column=1, sticky=(W, E))
+        
+        self._algo_select.bind(
+            "<<ComboboxSelected>>", self._handle_select_algo_field
+        )
 
     def _add_debug_borders(self, elem):
             elem["borderwidth"] = 3
