2c2cb466603b3139692529f3fac78859eea8747d
Antoine de Frahan <antoine.defrahan@outlook.com>
2023-03-19 19:09:15 +0100

    feat(gui): callbacks for algo options display

--- gui/sim.py
+++ gui/sim.py
@@ -7,6 +7,9 @@ class SimView(ttk.Frame):
     def __init__(self, master=None, **kwargs) -> None:
         super().__init__(master, **kwargs)
         self._setup()
+        self._algo_options_callbacks = dict()
+        self._algo_options_callbacks["Algorithme génétique"] = self._show_ga_options
+        self._algo_options_callbacks["PPO"] = self._show_ppo_options
 
     def _setup(self):
         # Environment selection
@@ -31,8 +34,7 @@ class SimView(ttk.Frame):
         self._creature_select_field.grid(row=1, column=1, sticky=(W, E))
 
         # Algorithm config
-        algo_config_lbl = ttk.Label(self, text="Configuration algorithme d'apprentissage")
-        #  self._add_debug_borders(algo_config_lbl)
+        self._algo_config_lbl = ttk.Label(self, text="Configuration algorithme d'apprentissage")
         self._algo_config_lbl.grid(row=2, column=0, padx="0 10", columnspan=2, sticky=W)
         self._algo_select_lbl = ttk.Label(self, text="Choix de l'algorithme")
         self._algo_select_lbl.grid(row=3, column=0, padx="0 10", sticky=W)
@@ -43,9 +45,18 @@ class SimView(ttk.Frame):
         self._algo_select_field["values"] = ["Algorithme génétique", "PPO"]
         self._algo_select_field.grid(row=3, column=1, sticky=(W, E))
         
-        self._algo_select.bind(
+        self._algo_select_field.bind(
             "<<ComboboxSelected>>", self._handle_select_algo_field
         )
+
+    def _handle_select_algo_field(self, ev):
+        self._algo_options_callbacks[self.__algo_var.get()]()
+
+    def _show_ga_options(self):
+        print("ga options")
+
+    def _show_ppo_options(self):
+        print("ppo options")
 
     def _add_debug_borders(self, elem):
             elem["borderwidth"] = 3
